9452d96b8ad2291e76b31d256a0e456740861a0e
Ashish Darji <ashdarji1@gmail.com>
2025-03-14 17:57:31 -0400

    updated some solid queue configuration and conversation views

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -3,6 +3,6 @@ Rails.application.config.solid_queue.configure do |config|
     
     # Configure the worker pool
     config.worker_pool = {
-      size: 5  # Sets the worker pool size
+      size: 15  # Sets the worker pool size
     }
   end
