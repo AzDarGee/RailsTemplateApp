d5ecb7986c21451ad3d848f661d66d02f922eea6
Ashish Darji <ashdarji1@gmail.com>
2025-05-14 14:51:35 -0400

    added admin boolean to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_05_194933) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_14_165729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -252,6 +252,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_05_194933) do
     t.string "uid"
     t.string "name"
     t.string "image"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
